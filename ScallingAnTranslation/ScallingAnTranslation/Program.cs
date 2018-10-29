aa0283dceb64f685d2793d18f5564b9a67a7b79c
svc-student <svc-student@student.directory.jccc>
2018-10-29 16:48:35 -0500

    New scalling and translation lab created.

---
+++ ScallingAnTranslation/ScallingAnTranslation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScallingAnTranslation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numVertices = 0;
+            int numVertex = 1;
+
+            Console.Write("How many vertices do you wish to enter? ");
+            numVertices = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+
+
+            do
+            {
+                Console.Write($"Enter the points of vertex {numVertex} separated by spaces: ");
+                string vertex = Convert.ToString(Console.ReadLine());
+                string[] vertexSplit = vertex.Split(' ');
+                
+
+            }
+        }
+    }
+}
